285c101205ae2ddb1239ee51183d500723cdeb7e
ayciceksamet <ayciceksamet@gmail.com>
2020-03-30 22:32:06 +0300

    created first django project

--- servicetracking/settings.py
+++ servicetracking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vs5sa)w)13zrdzzk6d(wk(hc-br6x1+4p9hh@u&&=deu7)1dke'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.20','127.0.0.1']
 
 
 # Application definition
